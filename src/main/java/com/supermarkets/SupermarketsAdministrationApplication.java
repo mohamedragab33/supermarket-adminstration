153d4e8f6544ff395feb1de6e68dbe83f887d4a1
mohamed ragab <moha.ragab33@gmail.com>
2022-03-09 18:39:20 +0200

    init

---
+++ src/main/java/com/supermarkets/SupermarketsAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.supermarkets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketsAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketsAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarkets/SupermarketsAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarkets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketsAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
