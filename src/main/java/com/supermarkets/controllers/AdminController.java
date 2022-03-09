295ea12ed820e46213d9b39da0e1246584928a9d
mohamed ragab <moha.ragab33@gmail.com>
2022-03-10 00:27:32 +0200

    1- Added mysql-connector dependency  2- Added Supermarket model  3- Added Database connection configuration to application properties file  4- Added Admin Controller to manage admin operations

---
+++ src/main/java/com/supermarkets/controllers/AdminController.java
@@ -0,0 +1,17 @@
+package com.supermarkets.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/api")
+public class AdminController {
+	
+	@GetMapping("/test")
+	public String  test() {
+		return "Test";
+	}
+	
+
+}
